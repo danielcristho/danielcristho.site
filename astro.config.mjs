8e5622e3dedf03e115315d62752e98dcb7b2a237
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-01-02 03:07:03 +0700

    chore: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,8 +52,8 @@ export default defineConfig({
             social: {
                 linkedin: "https://www.linkedin.com/in/daniel-pepuho/",
                 github: "https://github.com/danielcristho",
-                "x.com": "https://twitter.com/chrstdan",
-                rss: "https://medium.com/@danielpepuho"
+                "x.com": "https://twitter.com/chrstdan"
+                // rss: "https://medium.com/@danielpepuho"
             },
         }),
         compress(),
