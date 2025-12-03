983280db034f0dbe8a4b8c866e59826ce4a5a87a
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-12-03 20:02:08 +0700

    feat: remove flexloxi theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,58 +4,66 @@ import starlight from "@astrojs/starlight";
 import partytown from "@astrojs/partytown";
 import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
-import sitemap from '@astrojs/sitemap';
-import { BLOG_URL } from "./src/contants";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import starlightThemeFlexoki from 'starlight-theme-flexoki'
-import rss from '@astrojs/rss';
+import { BLOG_URL } from "./src/contants";
 
 export default defineConfig({
     site: BLOG_URL,
     integrations: [
-        tailwind(),
-        starlightThemeFlexoki(),
-        starlightBlog({
-            authors: {
-                danielcristho: {
-                    name: "Daniel Pepuho",
-                    url: "https://github.com/danielcristho",
-                },
+    tailwind(),
+
+    starlightBlog({
+        authors: {
+            danielcristho: {
+            name: "Daniel Pepuho",
+            url: "https://github.com/danielcristho",
             },
+        },
+        metrics: {
+            readingTime: true,
+            words: "total",
+        },
+    }),
+
+    starlight({
+        favicon: "/favicon.svg",
+        lastUpdated: true,
+        title: "danielcristho.",
+        editLink: {
+            baseUrl:
+            "https://github.com/danielcristho/danielcristho.site/edit/main",
+        },
+        customCss: [
+            "./src/styles/custom.css",
+            "./src/styles/gruvbox.css",
+
+        ],
+        components: {
+            MarkdownContent: "starlight-blog/overrides/MarkdownContent.astro",
+            Sidebar: "starlight-blog/overrides/Sidebar.astro",
+            TableOfContents: "./src/components/TableOfContents.astro",
+            Header: "./src/components/Header.astro",
+            Head: "./src/components/Head.astro",
+            Footer: "./src/components/Footer.astro",
+            Truncate: "./src/components/Truncate.astro",
+            SocialIcons: "./src/components/SocialIcons.astro",
+            ThemeSelect: "./src/components/starlight/ThemeSelect.astro",
+        },
+        social: {
+            linkedin: "https://www.linkedin.com/in/daniel-pepuho",
+            github: "https://github.com/danielcristho",
+            "x.com": "https://twitter.com/chrstdan",
+            rss: `${BLOG_URL}/rss.xml`,
+        },
         }),
-        starlight({
-            favicon: "/favicon.ico",
-            lastUpdated: true,
-            title: "danielcristho.",
-            editLink: {
-                baseUrl:
-                    "https://github.com/danielcristho/danielcristho.site/edit/main",
-            },
-            customCss: ["./src/styles/custom.css"],
-            components: {
-                MarkdownContent: "starlight-blog/overrides/MarkdownContent.astro",
-                Sidebar: "starlight-blog/overrides/Sidebar.astro",
-                TableOfContents: "./src/components/TableOfContents.astro",
-                Header: "./src/components/Header.astro",
-                Head: "./src/components/Head.astro",
-                Footer: "/src/components/Footer.astro",
-                Truncate: "/src/components/Truncate.astro",
-                SocialIcons: "./src/components/SocialIcons.astro",
-                ThemeSelect: "./src/components/starlight/ThemeSelect.astro",
-            },
-            social: {
-                linkedin: "https://www.linkedin.com/in/daniel-pepuho",
-                github: "https://github.com/danielcristho",
-                "x.com": "https://twitter.com/chrstdan",
-                rss: `${BLOG_URL}/rss.xml`,
-            },
-        }),
-        compress(),
-        robotsTxt(),
-        sitemap(),
-        partytown({
-            config: {
-                forward: ["dataLayer.push"],
+
+    compress(),
+    robotsTxt(),
+    sitemap(),
+    partytown({
+        config: {
+        forward: ["dataLayer.push"],
             },
         }),
     ],
